2eacd1c3da30331cd540c678353ca0a8d585c002
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-10-21 20:18:40 -0300

    Implementando o serviço que retornara as moedas

--- src/app/conversor/services/moeda.service.ts
+++ src/app/conversor/services/moeda.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
+import { Moeda } from '../models';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MoedaService {
 
+  private moedas : Moeda[];
+
   constructor() { }
+
+  private moedasObj = [    
+    {"sigla": "USD", "descricao": "Dólar"},    
+    {"sigla": "EUR", "descricao": "Euro"},
+    {"sigla": "BRL", "descricao": "Real"}
+  ];
+
+  listarTodas(): Moeda[]{
+
+    if (this.moedas) {
+      return this.moedas;
+    }
+
+    for (let moedaObj of this.moedasObj){
+      let moeda: Moeda = new Moeda();
+      Object.assign(moeda, moedaObj);
+      this.moedas.push(moeda);
+    }
+
+    return this.moedas;        
+
+  }   
+
 }
