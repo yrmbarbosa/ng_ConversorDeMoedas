36a9e71631dce38004be74a25785e2efaf9f5b19
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-10-21 20:23:35 -0300

    Criando o servico conversor, referenciando no componente e no modulo

--- src/app/conversor/components/conversor.component.spec.ts
+++ src/app/conversor/components/conversor.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConversorComponent } from './conversor.component';
-import { MoedaService } from '../services';
+import { MoedaService, ConversorService } from '../services';
 
 describe('ConversorComponent', () => {
   let component: ConversorComponent;
@@ -10,7 +10,7 @@ describe('ConversorComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ConversorComponent ],
-      providers: [ MoedaService ]
+      providers: [ MoedaService, ConversorService ]
     })
     .compileComponents();
   });
--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConversorComponent } from './components';
-import { MoedaService } from './services';
+import { MoedaService, ConversorService } from './services';
 
 @NgModule({
   declarations: [ConversorComponent],
@@ -13,7 +13,8 @@ import { MoedaService } from './services';
     ConversorComponent
   ],
   providers: [
-    MoedaService
+    MoedaService,
+    ConversorService
   ]
 })
 export class ConversorModule { }
