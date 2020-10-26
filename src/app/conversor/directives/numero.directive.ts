0aa3d321a4342d09ee86f44be1485abfcb4e294e
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-10-26 17:21:32 -0300

    Implementação da diretiva no HTML

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[numero]'
@@ -7,4 +7,21 @@ export class NumeroDirective {
 
   constructor() { }
 
+  @HostListener('keyup', ['$event'])
+
+  onKeyUp($event: any) {
+
+    let valor = $event.target.value;
+    let posDecimais = valor.indexOf('.');
+
+    valor = valor.replace(/[\D]/g, '');
+
+    if (posDecimais > 0) {
+      valor =  valor.substr(0, posDecimais) + '.' + valor.substr(posDecimais);
+    }
+
+    $event.target.value = valor;
+
+  }
+  
 }
