a2cb58798b8d488f5adb65001e87c54bee151e80
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-10-26 18:06:28 -0300

    Criando o componente modal de conversão de moedas

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -6,12 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
+import { ModalCotacaoComponent } from './utils';
 
 @NgModule({
-  declarations: [ConversorComponent, NumeroDirective],
+  declarations: [
+    ConversorComponent, 
+    NumeroDirective, 
+    ModalCotacaoComponent],
   imports: [
     CommonModule,
-    HttpClientModule  ,
+    HttpClientModule,
     FormsModule
   ],
   exports: [
---
+++ src/app/conversor/utils/modal-cotacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modal-cotacao',
+  templateUrl: './modal-cotacao.component.html',
+  styleUrls: ['./modal-cotacao.component.css']
+})
+export class ModalCotacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
