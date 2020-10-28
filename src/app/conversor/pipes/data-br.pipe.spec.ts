0183fbd28fb17b4542c1ac49862b812b6dcff248
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-10-28 13:35:14 -0300

    Implemento o filtro pipe de formatação para data brasileira

--- src/app/conversor/pipes/data-br.pipe.spec.ts
+++ src/app/conversor/pipes/data-br.pipe.spec.ts
@@ -1,8 +1,15 @@
 import { DataBrPipe } from './data-br.pipe';
 
 describe('DataBrPipe', () => {
+  
   it('create an instance', () => {
     const pipe = new DataBrPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('deve formatar a data 1990-04-20 para 20/04/1990', () => {
+    const pipe = new DataBrPipe();
+    expect(pipe.transform('1990-04-20')).toEqual('20/04/1990');
+  });
+
 });
--- src/app/conversor/pipes/data-br.pipe.ts
+++ src/app/conversor/pipes/data-br.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DataBrPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(dataEntrada: string): string {
+
+    if (!dataEntrada) {
+      return '';
+    }
+
+    const dataArr = dataEntrada.split('-');
+
+    if (dataArr.length !== 3) {
+      return dataEntrada;
+    }
+
+    return dataArr[2] + '/' + dataArr[1] + '/' + dataArr[0];
+
   }
 
 }
