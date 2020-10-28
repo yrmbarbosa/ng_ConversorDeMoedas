05163b158fbf651dfd7cbfc888e1219df7c34912
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-10-28 13:26:56 -0300

    Criando o filtro pipe de formatação de data no formato brasileiro

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -7,12 +7,13 @@ import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
 import { ModalCotacaoComponent } from './utils';
+import { DataBrPipe } from './pipes';
 
 @NgModule({
   declarations: [
     ConversorComponent, 
     NumeroDirective, 
-    ModalCotacaoComponent],
+    ModalCotacaoComponent, DataBrPipe],
   imports: [
     CommonModule,
     HttpClientModule,
--- src/app/conversor/index.ts
+++ src/app/conversor/index.ts
@@ -6,3 +6,4 @@ export * from './components';
 export * from './services';
 export * from './directives';
 export * from './utils';
+export * from './pipes';
